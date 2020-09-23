73a55acf93ce8e62b833ae04b1dbaccf64a92741
Ilya <1lys.rynd@gmail.com>
2020-09-23 18:36:34 +0300

    Add calc

--- main.cpp
+++ main.cpp
@@ -7,14 +7,23 @@
 
 using namespace std;
 
-int Sum(int a, int b){
-	return a + b;
-}
 int main(){
 	cout << "Hello World!!!";
 	int a , b;
 	cout << "Enter a and b";
 	cin >> a , b;
-	int res = Sum(a,b);
-	cout << "Sum = " << res << endl;
+	char op;
+	cout << "Enter the operation(+, -, *, /)" << endl;
+	cin >> op;
+	switch(op){
+		case '+':
+		cout <<Sum(a,b) << endl;
+		case '-':
+		cout <<Dif(a,b) << endl;
+		case '*':
+		cout <<Prod(a,b) << endl;
+		case '/':
+		cout <<Div(a,b) << endl;
+		
+	}
 }
